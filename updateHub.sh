a184f6af1777f75e737b8831cd8fdbf6e736a991
Sébastien Lucas <sebastien@slucas.fr>
2018-10-31 09:23:17 +0100

    Add the manifest and the update script

---
+++ updateHub.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+MAJMIN=`echo $TAG | cut -d . -f 1,2 `
+ARCH=`dpkg --print-architecture`
+dir="docker-rclone"
+
+echo $TAG
+echo $MAJMIN
+
+NBARCH=`manifest-tool inspect seblucas/$dir:$TAG | grep "Arch:" | wc -l`
+if [[ $NBARCH -gt 1 ]]; then
+  echo "Image seblucas/$dir:$TAG already uptodate"
+  exit 0
+fi
+docker build . -t seblucas/$dir:$ARCH-$TAG -t seblucas/$dir:$MAJMIN
+docker push seblucas/$dir:$ARCH-$TAG
+if [[ $ARCH == "armhf" ]]; then
+  echo "Manifest uploading ..."
+  sed -i "s|{image}|$dir|g" manifest.yaml
+  sed -i "s|{tag}|$TAG|g" manifest.yaml
+  sed -i "s|{majmin}|$MAJMIN|g" manifest.yaml
+  manifest-tool push from-spec manifest.yaml
+  git checkout manifest.yaml
+fi
